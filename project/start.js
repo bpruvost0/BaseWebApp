e6819b90a9dc194db6331c6d4d8759198a548306
bpruvost <bpruvost@hubspot.com>
2018-11-19 17:54:50 +0000

    Personal Website

--- project/start.js
+++ project/start.js
@@ -9,9 +9,23 @@ app.use(express.static(__dirname));
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
+// Set up the app's routing
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
